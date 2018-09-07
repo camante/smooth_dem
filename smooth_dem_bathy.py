a164474e88bb18660991acf56f8728ddcdd645dc
Matthew Love <matthew.love@colorado.edu>
2018-09-06 19:15:18 -0700

    batch list

--- smooth_dem_bathy.py
+++ smooth_dem_bathy.py
@@ -16,20 +16,27 @@ version %s
 _usage = """
 smooth_dem_bathy.py: A script that smooths the bathy areas of DEM (below 0) and merges back with original, unsmoothed topo.
 
-usage: smooth_dem_bathy.py input-dem [-smooth value]
+usage: smooth_dem_bathy.py [ si [ args ] ] [ file ]
 
 Options:
-  input-dem\tThe input DEM file-name
-  -smooth\tValue for the smooth factor; default is 10
+  file\t\tThe input DEM file-name
+  -s\t\tValue for the smooth factor; default is 10
+  -i\t\tA file containing DEM file-names to process
 
   -help\t\tPrint the usage text
   -version\tPrint the version information
 
 Example:
-smooth_dem_bathy.py input.tif -smooth 12
+smooth_dem_bathy.py input.tif -s 12
 
 smooth_dem_bathy.py v.%s 
 """ %(_version)
+
+def open_file_list(in_list, smooth_factor):
+    il = open(in_list, 'r')
+    for line in il:
+        proc_elev(line.strip(), smooth_factor)
+    il.close()
 
 def gaussian_blur(in_array, size):
     # expand in_array to fit edge of kernel
@@ -79,10 +86,50 @@ def CreateGeoTiff(Name, Array, driver,
     #DataSet.GetRasterBand(1).SetNoDataValue(NDV)
     return NewFileName
 
+def proc_elev(elev, smooth_factor):
+    if not os.path.exists(elev):
+        print("Error: %s is not a valid file" %(elev))
+    else:
+        #Create Array
+        output_name=elev[:-4]+"_smooth_"+str(smooth_factor)
+        xsize, ysize, GeoT, Projection, DataType, NDV = GetGeoInfo(elev)
+        
+        print "elev is", elev
+        print "smooth factor is", smooth_factor
+        print "output_name is", output_name
+        
+        elev_g = gdal.Open(elev) #
+        elev_array = elev_g.GetRasterBand(1).ReadAsArray(0,0,xsize,ysize) 
+        mask_array = elev_array
+        elev_array = None
+        #Set topo values to zero
+        mask_array[mask_array > 0] = 0
+        print "loaded input dem"
+
+        #Perform smoothing
+        smooth_elev=gaussian_blur(mask_array, smooth_factor)
+        mask_array[mask_array < 0] = 1
+        smooth_elev = smooth_elev * mask_array
+        mask_array = None
+        print "smoothed array"
+    
+        #Reload original array and merge the topo with the smoothed bathy
+        elev_array = elev_g.GetRasterBand(1).ReadAsArray(0,0,xsize,ysize)
+        elev_array[elev_array < 0] = 0
+        smoothed_array = smooth_elev + elev_array
+        elev_g = elev_array = smooth_elev = None
+        
+        #Export Tif
+        driver = gdal.GetDriverByName('GTiff')
+        CreateGeoTiff(output_name, smoothed_array, driver, xsize, ysize, GeoT, Projection, DataType)
+        smoothed_array = None
+        print "created Smoothed Geotiff"
+
 if __name__ == '__main__':
     
     elev = None
     smooth_factor = 10
+    in_list = None
 
     i = 1
     while i < len(sys.argv):
@@ -90,6 +137,10 @@ if __name__ == '__main__':
 
         if arg == '-s' or arg == '-smooth' or arg == '--smooth':
             smooth_factor = sys.argv[i+1]
+            i = i + 1
+
+        if arg == '-i':
+            in_list = sys.argv[i+1]
             i = i + 1
 
         elif arg == '-help' or arg == '--help' or arg == '-h':
@@ -110,52 +161,15 @@ if __name__ == '__main__':
 
         i = i + 1
 
-    if elev is None:
+    if elev is None and in_list is None:
         print(_usage)
         sys.exit(1)
 
-    if not os.path.exists(elev):
-        print("Error: %s is not a valid file" %(elev))
-        print(_usage)
-        sys.exit(1)
-    
     try: smooth_factor = int(smooth_factor)
     except:
         print("Error: %s is not a valid smooth-factor" %(smooth_factor))
         print(_usage)
         sys.exit(1)
 
-    #Create Array
-    output_name=elev[:-4]+"_smooth_"+str(smooth_factor)
-    xsize, ysize, GeoT, Projection, DataType, NDV = GetGeoInfo(elev)
-
-    print "elev is", elev
-    print "smooth factor is", smooth_factor
-    print "output_name is", output_name
-
-    elev_g = gdal.Open(elev) #
-    elev_array = elev_g.GetRasterBand(1).ReadAsArray(0,0,xsize,ysize) 
-    mask_array = elev_array
-    elev_array = None
-    #Set topo values to zero
-    mask_array[mask_array > 0] = 0
-    print "loaded input dem"
-
-    #Perform smoothing
-    smooth_elev=gaussian_blur(mask_array, smooth_factor)
-    mask_array[mask_array < 0] = 1
-    smooth_elev = smooth_elev * mask_array
-    mask_array = None
-    print "smoothed array"
-    
-    #Reload original array and merge the topo with the smoothed bathy
-    elev_array = elev_g.GetRasterBand(1).ReadAsArray(0,0,xsize,ysize)
-    elev_array[elev_array < 0] = 0
-    smoothed_array = smooth_elev + elev_array
-    elev_g = elev_array = smooth_elev = None
-
-    #Export Tif
-    driver = gdal.GetDriverByName('GTiff')
-    CreateGeoTiff(output_name, smoothed_array, driver, xsize, ysize, GeoT, Projection, DataType)
-    smoothed_array = None
-    print "created Smoothed Geotiff"
+    if in_list: open_file_list(in_list, smooth_factor)
+    else: proc_elev(elev, smooth_factor)
